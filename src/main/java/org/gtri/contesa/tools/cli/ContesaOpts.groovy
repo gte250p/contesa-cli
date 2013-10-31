576a63e94006c576308c44239b6109a3945846ce
Brad Lee <s.brad.lee@gmail.com>
2013-10-31 10:54:53 -0400

    Added ability to add logfile and outfile paths to options, and tested that it works as expected

--- src/main/java/org/gtri/contesa/tools/cli/ContesaOpts.groovy
+++ src/main/java/org/gtri/contesa/tools/cli/ContesaOpts.groovy
@@ -23,6 +23,18 @@ class ContesaOpts {
      * The location of the instance to validate on disk.
      */
     public String instancePath = null;
+    public File instanceFile = null;
+
+    /**
+     * The output file to write result XML into.  May be null, in which case it will go to standard out.
+     */
+    public String outFilePath = null;
+
+    /**
+     * The output file to write contesa logging into.  May be null, in which case it will go to standard out/err.  It
+     * is recommended that you suppress logging when outputting a file to the console.
+     */
+    public String logFilePath = null;
 
 
     public ContesaOpts(String[] args){
@@ -32,21 +44,55 @@ class ContesaOpts {
                 arg.chars.each { c ->
                     if( c == 'v' ){
                         verbosity++;
+                        if( verbosity > 4 ){
+                            throw new Exception("Only 4 levels of verbose are supported.")
+                        }
                     }
                 }
-            }else if( arg.equalsIgnoreCase("-ne") || arg.equalsIgnoreCase("--no-error-on-no-match") ){
+
+            }else if( arg.startsWith("-o=") || arg.toLowerCase().startsWith("--out-file=") ){
+                outFilePath = parseArgValue(arg);
+
+            }else if( arg.startsWith("-l=") || arg.toLowerCase().startsWith("--log=") ){
+                logFilePath = parseArgValue(arg);
+
+            }else if( arg.equals("-ne") || arg.equalsIgnoreCase("--no-error-on-no-match") ){
                 errorOnNoMatch = Boolean.FALSE;
+
             }else if( arg.startsWith("-") ){
                 throw new Exception("Unrecognized option: ${arg}");
+
             }else{
-                if( !instancePath )
+                if( !instancePath ){
                     instancePath = arg;
-                else
+                    instanceFile = resolvePath(instancePath);
+                }else
                     throw new Exception("Cannot contain more than one file path to validate.  The system saw ${instancePath} first.")
             }
         }
         INSTANCE = this;
+    }//end public constructor
+
+    protected File resolvePath(String path){
+        return resolvePath(path, false);
     }
+
+    protected File resolvePath(String path, Boolean isDirectory){
+        File file = new File(path);
+        if( !file.exists() )
+            throw new FileNotFoundException("Could not locate file: $path");
+        if( !isDirectory && file.isDirectory() )
+            throw new Exception("Expecting a file, but found a directory at: $path")
+        return file;
+    }
+
+    protected String parseArgValue( String arg ){
+        def index1 = arg.indexOf('=') + 1
+        def rest = arg.substring(index1);
+        if( rest.trim().length() == 0 )
+            throw new Exception("Argument '$arg' requires a value.")
+        return rest.trim();
+    }//end parseArgValue()
 
 
 }//end ContesaOpts
--- src/test/java/org/gtri/contesa/tools/cli/TestContesaOpts.groovy
+++ src/test/java/org/gtri/contesa/tools/cli/TestContesaOpts.groovy
@@ -1,5 +1,6 @@
 package org.gtri.contesa.tools.cli
 
+import org.junit.Assert
 import org.junit.Test
 import static org.hamcrest.Matchers.*
 import static org.hamcrest.MatcherAssert.*
@@ -12,24 +13,37 @@ import static org.hamcrest.MatcherAssert.*
  */
 class TestContesaOpts extends AbstractTest {
 
+    static String POM_XML = "pom.xml"
+    static String ASSEMBLY_XML = "./src/main/assembly/dist.xml"
 
     private void assertDefaults(ContesaOpts opts){
+        assertValues(opts, 0, Boolean.TRUE, null);
+    }
+
+    private void assertDefaults(ContesaOpts opts, String path){
+        assertValues(opts, 0, Boolean.TRUE, path);
+    }
+
+    private void assertValues(ContesaOpts opts, Integer verbosity, Boolean errorOnNoMatch, String instancePath){
         assertThat(opts, notNullValue())
         assertThat(opts.verbosity, notNullValue())
-        assertThat(opts.verbosity, equalTo(0))
+        assertThat(opts.verbosity, equalTo(verbosity))
         assertThat(opts.errorOnNoMatch, notNullValue())
-        assertThat(opts.errorOnNoMatch, equalTo(Boolean.TRUE))
-        assertThat(opts.instancePath, notNullValue())
+        assertThat(opts.errorOnNoMatch, equalTo(errorOnNoMatch))
+        if( instancePath ){
+            assertThat(opts.instancePath, notNullValue())
+            assertThat(opts.instancePath, equalTo(instancePath))
+        }else{
+            assertThat(opts.instancePath, nullValue())
+        }
     }
 
     @Test
     public void testOptionParsingForFileOnly() {
         logger.info("Testing that we can parse file only arguments correctly...")
-        def PATH1 = "/a/path/to/file"
-        def args = [PATH1] as String[];
+        def args = [POM_XML] as String[];
         ContesaOpts opts = new ContesaOpts(args);
-        assertDefaults(opts);
-        assertThat(opts.instancePath, equalTo(PATH1))
+        assertDefaults(opts, POM_XML);
         logger.info("Successfully tested single file arg only case.")
     }//end testOptionParsingForFileOnly()
 
@@ -40,22 +54,146 @@ class TestContesaOpts extends AbstractTest {
 
         def args = ["-v"] as String[];
         ContesaOpts opts = new ContesaOpts(args);
-        assertThat(opts.verbosity, equalTo(1))
+        assertValues(opts, 1, Boolean.TRUE, null);
 
         args = ["-vv"] as String[];
         opts = new ContesaOpts(args);
-        assertThat(opts.verbosity, equalTo(2))
+        assertValues(opts, 2, Boolean.TRUE, null);
 
         args = ["-vvv"] as String[];
         opts = new ContesaOpts(args);
-        assertThat(opts.verbosity, equalTo(3))
+        assertValues(opts, 3, Boolean.TRUE, null);
 
         args = ["-vvvv"] as String[];
         opts = new ContesaOpts(args);
-        assertThat(opts.verbosity, equalTo(4))
+        assertValues(opts, 4, Boolean.TRUE, null);
+
+        args = ["-vvvvv"] as String[];
+        try{
+            opts = new ContesaOpts(args);
+            Assert.fail("Expecting error when verbose level is above 4, but no error occurred!")
+        }catch(Exception e){
+            assertThat(e.message, equalTo("Only 4 levels of verbose are supported."))
+        }
 
         logger.info("Successfully tested verbosity in arguments.")
     }//end testVerbosityLevelsInArgs()
 
+    @Test
+    public void testNoErrorSetInArgs() {
+        logger.info("Testing that the argument to switch errors off when no match contexts are found works...")
+
+        def args = ["-ne"] as String[];
+        ContesaOpts opts = new ContesaOpts(args);
+        assertValues(opts, 0, Boolean.FALSE, null);
+
+        args = [] as String[];
+        opts = new ContesaOpts(args);
+        assertDefaults(opts)
+
+        args = ["--no-error-on-no-match"] as String[];
+        opts = new ContesaOpts(args);
+        assertValues(opts, 0, Boolean.FALSE, null);
+
+        logger.info("No error tested ok.");
+    }//end testNoErrorSetInArgs()
+
+    @Test
+    public void testOnly1InstanceArgumentAllowed() {
+        logger.info("Testing that only 1 instance path argument is allowed...")
+
+        def args = [POM_XML] as String[];
+        ContesaOpts opts = new ContesaOpts(args);
+        assertDefaults(opts, POM_XML);
+
+        try{
+            args = [POM_XML, ASSEMBLY_XML] as String[];
+            opts = new ContesaOpts(args)
+            Assert.fail("Expecting error on 2 instance paths, but that did not occur.")
+        }catch(Exception e){
+            assertThat(e.message, startsWith("Cannot contain more than one file path to validate"))
+        }
+
+        logger.info("Successfully verified only 1 instance path is allowed.")
+    }//end testOnly1InstanceArgumentAllowed()
+
+
+    @Test
+    public void testSetLogFilePath() {
+        logger.info("Testing that setting log file path works ok...");
+
+        def args = ["-l=/path/1"] as String[];
+        ContesaOpts opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.logFilePath, notNullValue())
+        assertThat(opts.logFilePath, equalTo("/path/1"))
+
+        args = ["--log=/path/1"] as String[];
+        opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.logFilePath, notNullValue())
+        assertThat(opts.logFilePath, equalTo("/path/1"))
+
+        args = ["--LOG=/path/1"] as String[];
+        opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.logFilePath, notNullValue())
+        assertThat(opts.logFilePath, equalTo("/path/1"))
+
+        logger.info("Successfully Tested that setting log file path works ok");
+    }//end testSetLogFilePath()
+
+    @Test
+    public void testSetOutFilePath() {
+        logger.info("Testing that setting out file path works ok...");
+
+        def args = ["-o=/path/1"] as String[];
+        ContesaOpts opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.outFilePath, notNullValue())
+        assertThat(opts.outFilePath, equalTo("/path/1"))
+
+        args = ["--out-file=/path/1"] as String[];
+        opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.outFilePath, notNullValue())
+        assertThat(opts.outFilePath, equalTo("/path/1"))
+
+        args = ["--OUT-FILE=/path/1"] as String[];
+        opts = new ContesaOpts(args);
+        assertDefaults(opts);
+        assertThat(opts.outFilePath, notNullValue())
+        assertThat(opts.outFilePath, equalTo("/path/1"))
+
+        logger.info("Successfully tested that setting out file path works ok");
+    }//end testSetLogFilePath()
+
+
+    @Test
+    public void testCommonOptionCombinations() {
+        logger.info("Testing that common option combinations should work ok.");
+
+        // Simple single path option (all defaults except path)
+        def args = [ASSEMBLY_XML] as String[];
+        ContesaOpts opts = new ContesaOpts(args);
+        assertDefaults(opts, ASSEMBLY_XML)
+
+        // Simple "verbose" option
+        args = ["-v", POM_XML] as String[];
+        opts = new ContesaOpts(args);
+        assertValues(opts, 1, Boolean.TRUE, POM_XML);
+
+        // Simple no-error and path options
+        args = ["-ne", POM_XML] as String[];
+        opts = new ContesaOpts(args);
+        assertValues(opts, 0, Boolean.FALSE, POM_XML);
+
+        // All arguments
+        args = ["-v", "-ne", ASSEMBLY_XML] as String[];
+        opts = new ContesaOpts(args);
+        assertValues(opts, 1, Boolean.FALSE, ASSEMBLY_XML);
+
+        logger.info("Common options combinations seems fine.")
+    }//end testCommonOptionCombinations()
 
 }//end TestValidationCLI()
